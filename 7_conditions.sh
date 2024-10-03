3f058cf42a09bd6032c1ec72d1f8f0bac4045f73
root <root@ip-172-31-40-18.eu-north-1.compute.internal>
2024-10-03 17:35:34 +0000

    second commit

--- 7_conditions.sh
+++ 7_conditions.sh
@@ -3,10 +3,10 @@
 #This is for loops we need to use loops 
 #
 #
-#We will us If - Else loops
+#We will us If - Else loops ========================================================
 #
-
-read -p "enter the number" mark
+<<comment
+read -p "enter the number:........." mark
 
 if  [ $mark -gt 40 ] # there should be space in the starting of the sysntax
 	# "-gt" :  means greather then 
@@ -20,7 +20,7 @@ then
 else
 	echo "fail"
 fi
-# elif loop
+# elif loop ==============================================================================================================
 #
 
 if [ $mark -ge 80 ]
@@ -33,3 +33,71 @@ then
 else
 	echo "poora"
 fi
+
+# Case ====================================================================================================================
+echo " a) to kmow the time "
+echo" b) to know the data "
+echo "c)to know availablwe files"
+echo  " d) free momory"
+
+read -p "..................." king # we will use  " -p" to add some info in the same line"
+read -p "second value" king
+case $king in
+	a)date;;
+	d)                              # this is the other way to declar 
+		echo today date is           
+		date
+		;;                             # this is the ending
+	b)ls;;
+	c)pwd;;
+	*)echo provide a value
+esac
+
+# operator ======================================================================
+#
+#
+read -p "enter the age : " age
+read -p "enetr the country: " country
+if [[ $age -ge 18 ]] && [[ $country -eq "india" ]] && [[ $age -lt 80 ]] # for three state ments
+then
+	echo "vpote"
+else
+	echo "no vote"
+fi
+comment
+========================================
+read -p "enter the age : " age
+read -p "enetr the country: " country
+if [[ $age -ge 18 ]] || [[ $country -eq "india" ]] # there should be gap between the [[  ]]
+then
+        echo "vpote"
+else
+        echo "no vote"
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
